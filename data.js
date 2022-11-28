50958c4e1fc26069c06c02025cacd8651928cff7
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2022-11-28 14:24:52 -0600

    all new features
    I make it responsive and like and unlike

---
+++ data.js
@@ -0,0 +1,19 @@
+export const likesObjects = [{
+    Image: 'images/post-vangogh.jpg',
+    likes: 21492,
+    isLike: false,
+    uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee'
+
+},
+{
+    Image: 'images/avatar-courbet.jpg',
+    likes: 12590,
+    isLike: false,
+    uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2'
+},
+{   Image: 'images/post-ducreux.jpg',
+    likes: 5290,
+    isLike: false,
+    uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45'
+}
+]
--- index.js
+++ index.js
@@ -1,28 +1,69 @@
-const likeEl = document.getElementById("like-btn")
-const likeEl2 = document.getElementById("like-btn2")
-const likeEl3 = document.getElementById("like-btn3")
-const likesCount = document.getElementById("likes-one")
-const likesCount2 = document.getElementById("likes-two")
-const likesCount3 = document.getElementById("likes-three")
+import { likesObjects } from "./data.js"
 
 
-let likes = 290
-let likes2 = 275
-let likes3 = 212
 
-likeEl.addEventListener("click", () => {
-    likes += 1
-    likesCount.textContent = `${likes} likes `
+
+document.addEventListener('click', (e) => {
+   if(e.target.dataset.like) {
+    handleLikeClik(e.target.dataset.like)
+   }
+})
+
+
+//like function
+function handleLikeClik(likeId) {
+    const targetLikeObj = likesObjects.filter((postLikes) => {
+        return postLikes.uuid === likeId
+    })[0]
     
-}) 
+    if(targetLikeObj.isLike) {
+        targetLikeObj.likes--
+    } else {
+        targetLikeObj.likes++
+        
+    }
+    //flip boolean
+    targetLikeObj.isLike = !targetLikeObj.isLike
+    render()
+}
 
-likeEl2.addEventListener("click", () => {
-    likes2 += 1
-    likesCount2.textContent = `${likes2} likes`
-})
+// main html contents
+function getFeedHtml() {
+    let feedHtml = ``
+    likesObjects.forEach((post) => {
+        let likeHeart = ''
+        //add color to like and unlike
+        if(post.isLike) {
+            likeHeart = 'like-color'
+        }
 
-likeEl3.addEventListener("click", () => {
-    likes3 += 1
-    likesCount3.textContent = `${likes3} likes`
-})
+        feedHtml +=` 
+        <section>
+        <div class="user-info">
+            <img src="images/avatar-vangogh.jpg" alt="user-pics" class="user-pics">
+            <h3>Vincent van Gogh <span class="country"><p>Zundert, Netherlands</p></span></h3>
+        </div>
+        <img src="${post.Image}" alt="vangogh-image" class="post-image">
+        <div class="like-section">
+        <i class="fa-solid fa-heart ${likeHeart}" data-like="${post.uuid}"></i>
+        <i class="fa-regular fa-comment"></i>
+        <i class="fa-regular fa-paper-plane"></i>
+        <h3 class="likes">${post.likes} likes</h3>
+        <h4>Adeolu777<span class="comments"> just took a few mushrooms lol</span></h4>
+        </div>
+    </section>      
+        `
+    })
+    return feedHtml
+}
+
+function render() {
+    document.getElementById('main-content').innerHTML = getFeedHtml()
+}
+render()
+
+
+
+
+
 
